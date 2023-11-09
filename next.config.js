f57bb9eed68653bc522be6b5b1f3c5b8dcdfa464
Fran <fducekuce@gmail.com>
2023-11-09 20:13:35 +0100

    add filter

--- next.config.js
+++ next.config.js
@@ -3,6 +3,30 @@ const nextConfig = {
   images: {
     domains: ["image.tmdb.org"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
